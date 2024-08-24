44d312950b3836e08c3573d20b1814c38a1ec88a
Valentin Dmitriev <vald1007@gmail.com>
2024-08-24 11:31:43 +0500

    The using namespace Declaration

---
+++ Lesson 02/listing02_02.cpp
@@ -0,0 +1,15 @@
+// Preprocessor directive
+#include <iostream>
+
+// Start your program
+int main()
+{
+	// Tell the compiler what namespace to search in
+	using namespace std;
+
+	/* Write to the screen using std::cout */
+	cout << "Hello World" << endl;
+
+	// Return a value to the OS
+	return 0;
+}
