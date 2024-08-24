e7c3f460b4bac08a4d2ba0e98d1569c8f3dcad93
Valentin Dmitriev <vald1007@gmail.com>
2024-08-24 20:10:11 +0500

    Tell What Days of the Week Are Named After Using switch-case, break, and default

--- Lesson 06/listing06_05.cpp
+++ Lesson 06/listing06_05.cpp
@@ -20,22 +20,32 @@ int main()
 	int dayInput = SUNDAY; // Initialize to Sunday
 	cin >> dayInput;
 
-	if (dayInput == SUNDAY)
-		cout << "Sunday was named after the Sun" << endl;
-	else if (dayInput == MONDAY)
-		cout << "Monday was named after the Moon" << endl;
-	else if (dayInput == TUESDAY)
-		cout << "Tuesday was named after Mars" << endl;
-	else if (dayInput == WEDNESDAY)
-		cout << "Wednesday was named after Mercury" << endl;
-	else if (dayInput == THURSDAY)
-		cout << "Thursday was named after Jupiter" << endl;
-	else if (dayInput == FRIDAY)
-		cout << "Friday was named after Venus" << endl;
-	else if (dayInput == SATURDAY)
-		cout << "Saturday was named after Saturn" << endl;
-	else
-		cout << "Wrong input, execute again" << endl;
-
+	switch (dayInput)
+	{
+		case SUNDAY:
+			cout << "Sunday was named after the Sun" << endl;
+			break;
+		case MONDAY:
+			cout << "Monday was named after the Moon" << endl;
+			break;
+		case TUESDAY:
+			cout << "Tuesday was named after Mars" << endl;
+			break;
+		case WEDNESDAY:
+			cout << "Wednesday was named after Mercury" << endl;
+			break;
+		case THURSDAY:
+			cout << "Thursday was named after Jupiter" << endl;
+			break;
+		case FRIDAY:
+			cout << "Friday was named after Venus" << endl;
+			break;
+		case SATURDAY:
+			cout << "Saturday was named after Saturn" << endl;
+			break;
+		default:
+			cout << "Wrong input, execute again" << endl;
+			break;
+	}
 	return 0;
 }
