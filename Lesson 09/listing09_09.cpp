28e249ed8ffb31f0d160571eaa0389f698cb914b
Valentin Dmitriev <vald1007@gmail.com>
2024-08-26 09:17:23 +0500

    Define a Copy Constructor to Ensure Deep Copy of Dynamically Allocated Buffers

---
+++ Lesson 09/listing09_09.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string.h>
+using namespace std;
+class MyString
+{
+public:
+	MyString(const char* initString)
+	{
+		buffer = NULL;
+		cout << "Default constructor: creating new MyString" << endl;
+		if (initString != NULL)
+		{
+			buffer = new char[strlen(initString) + 1];
+			strcpy(buffer, initString);
+			
+			cout << "buffer points to: 0x" << hex;
+			cout << (unsigned int*)buffer << endl;
+		}
+
+	}
+	MyString(const MyString& copySource) // copy constructor
+	{
+		buffer = NULL;
+		cout << "Copy constructor: copying from MyString" << endl;
+		if (copySource.buffer != NULL)
+			// allocate own buffer
+			buffer = new char[strlen(copySource.buffer) + 1];
+
+			// deep copy from the source into local buffer
+			strcpy(buffer, copySource.buffer);
+
+			cout << "buffer points to: 0x" << hex;
+			cout << (unsigned int*)buffer << endl;
+	}
+	// Destructor
+	~MyString()
+	{
+		cout << "Invoking destructor, clearing up" << endl;
+		delete[] buffer;
+	}
+
+	int GetLength()
+	{
+		return strlen(buffer);
+	}
+	const char* GetString()
+	{
+		return buffer;
+	}
+private:
+	char* buffer;
+};
+
+void UseMyString(MyString str)
+{
+	cout << "String buffer in MyString is " << str.GetLength();
+	cout << " characters long" << endl;
+
+	cout << "buffer contains: " << str.GetString() << endl;
+	return;
+}
+int main()
+{
+	MyString sayHello("Hello from String Class");
+	UseMyString(sayHello);
+
+	return 0;
+}
